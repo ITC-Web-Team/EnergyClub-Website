3e87e76ae1762d617c30f2a1a3e26e4f68d9162d
deepaksilaych <deepaksilaych@gmail.com>
2024-11-16 18:11:49 +0530

    a

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -14,6 +15,7 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
       },
     ],
+    unoptimized: true, 
   },
 };
 
